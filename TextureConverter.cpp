61981657d073a940d0bcadb50fef7c8a24f50f71
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-10-16 14:51:56 +0900

    テクスチャをWICからDDSに変換する関数を、宣言だけしておく

--- TextureConverter.cpp
+++ TextureConverter.cpp
@@ -1,1 +1,6 @@
 #include "TextureConverter.h"
+
+void TextureConverter::ConvertTextureWICToDDS(const std::string& filePath) {
+	//①テクスチャファイルを読み込む
+	//②DDS形式に変換して書き込む
+}
--- TextureConverter.h
+++ TextureConverter.h
@@ -1,7 +1,13 @@
 /*テクスチャコンバータ*/
 #pragma once
+#include <string>
 
 //テクスチャコンバータ
 class TextureConverter {
-
+public:
+	/// <summary>
+	/// テクスチャをWICからDDSに変換
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void ConvertTextureWICToDDS(const std::string& filePath);
 };
