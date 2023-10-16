4a8e5114576961eed44ea5fa00317f99a1e6d8ec
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-10-16 14:52:36 +0900

    宣言した関数を、mainで事前に呼び出しておく

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cassert>
+
+#include "TextureConverter.h"
 
 //コマンドライン
 enum Argument {
@@ -10,13 +13,22 @@ enum Argument {
 };
 
 int main(int argc, char* argv[]) {
+/*
 	//argcの数だけ繰り返す
 	for (int i = 0; i < argc; i++) {
 		//文字列argvのi番を表示
 		printf(argv[i]);
 		printf("\n");
 	}
+*/
 	
+	assert(argc >= NumArgument_);
+	//テクスチャコンバータ
+	TextureConverter converter;
+
+	//テクスチャ変換
+	converter.ConvertTextureWICToDDS(argv[kFilePath]);
+
 	system("pause");
 	return 0;
 }
