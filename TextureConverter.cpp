4fdafe30bb5cdb6443ec4d80131458a09cf37c3d
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-11-20 14:34:23 +0900

    ワイド文字列への変換

--- TextureConverter.cpp
+++ TextureConverter.cpp
@@ -1,4 +1,6 @@
 #include "TextureConverter.h"
+#include <stringapiset.h>
+
 
 void TextureConverter::ConvertTextureWICToDDS(const std::string& filePath) {
 	//①テクスチャファイルを読み込む
@@ -13,5 +15,16 @@ void TextureConverter::LoadWICTextureFromFile(const std::string& filePath) {
 }
 
 std::wstring TextureConverter::ConvertMultiByteStringToWideString(const std::string& mString) {
+	//ワイド文字列に変換した際の文字数
+	int bufferSize = 
+		MultiByteToWideChar(CP_UTF8, 0, mString.c_str(), -1, nullptr, 0);
+
+	//ワイド文字列
+	std::wstring wString;
+	wString.resize(bufferSize);
+
+	//変換する
+	MultiByteToWideChar(CP_ACP, 0, mString.c_str(), -1, &wString[0], bufferSize);
+
 	return std::wstring();
 }
