beae0c323a65010639cd5982c4210d63c5ca36eb
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-11-28 14:39:40 +0900

    WICテクスチャを読み込む

--- TextureConverter.cpp
+++ TextureConverter.cpp
@@ -1,6 +1,6 @@
 #include "TextureConverter.h"
 
-#include <Windows.h>
+using namespace DirectX;
 
 void TextureConverter::ConvertTextureWICToDDS(const std::string& filePath) {
 	//①テクスチャファイルを読み込む
@@ -10,23 +10,29 @@ void TextureConverter::ConvertTextureWICToDDS(const std::string& filePath) {
 }
 
 void TextureConverter::LoadWICTextureFromFile(const std::string& filePath) {
+	HRESULT result;
+
 	//①ファイルパスをワイド文字列に変換
 	std::wstring wfilePath = ConvertMultiByteStringToWideString(filePath);
 
 	//②テクスチャを読み込む
+	result = LoadFromWICFile(wfilePath.c_str(), WIC_FLAGS_NONE, &metadata_, scratchImage_
+	);
+
+	assert(SUCCEEDED(result));
 }
 
 std::wstring TextureConverter::ConvertMultiByteStringToWideString(const std::string& mString) {
 	//ワイド文字列に変換した際の文字数
 	int bufferSize = 
-		MultiByteToWideChar(CP_UTF8, 0, mString.c_str(), -1, nullptr, 0);
+		MultiByteToWideChar(CP_ACP, 0, mString.c_str(), -1, nullptr, 0);
 
 	//ワイド文字列
 	std::wstring wString;
 	wString.resize(bufferSize);
 
 	//変換する
-	MultiByteToWideChar(CP_UTF8, 0, mString.c_str(), -1, &wString[0], bufferSize);
+	MultiByteToWideChar(CP_ACP, 0, mString.c_str(), -1, &wString[0], bufferSize);
 
 	return wString;
 }
