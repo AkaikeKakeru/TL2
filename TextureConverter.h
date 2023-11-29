771cbad638628e8258337381e4e7a7a96561f2ec
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-11-29 16:52:32 +0900

    ワイド文字列に変換した後のファイルパスを、「ディレクトリパス」「ファイル名」「拡張子」に分解する関数を宣言。使う予定の変数も宣言。

--- TextureConverter.h
+++ TextureConverter.h
@@ -26,10 +26,22 @@ private:
 	/// <returns>ワイド文字列</returns>
 	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
 
+	/// <summary>
+	/// フォルダパスとファイル名を分解する
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void SeparateFilePath(const std::wstring& filePath);
+
 private:
 	//画像の情報
 	DirectX::TexMetadata metadata_;
-
 	//画像イメージのコンテナ
 	DirectX::ScratchImage scratchImage_;
+
+	//ディレクトリパス
+	std::wstring directoryPath_;
+	//ファイル名
+	std::wstring fileName_;
+	//ファイル拡張子
+	std::wstring fileExt_;
 };
