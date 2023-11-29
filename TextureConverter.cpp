a0fbc0dd8403e4d83a40e60e9c9bec658d3a0797
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-11-29 17:19:57 +0900

    ファイルパス分解関数の中身を定義

--- TextureConverter.cpp
+++ TextureConverter.cpp
@@ -14,10 +14,10 @@ void TextureConverter::LoadWICTextureFromFile(
 	const std::string& filePath) {
 	HRESULT result;
 
-	//①ファイルパスをワイド文字列に変換
+	//ファイルパスをワイド文字列に変換
 	std::wstring wfilePath = ConvertMultiByteStringToWideString(filePath);
 
-	//②テクスチャを読み込む
+	//テクスチャを読み込む
 	result = LoadFromWICFile(
 		wfilePath.c_str(),
 		WIC_FLAGS_NONE,
@@ -26,6 +26,9 @@ void TextureConverter::LoadWICTextureFromFile(
 	);
 
 	assert(SUCCEEDED(result));
+
+	//フォルダパスとファイル名を分解
+	SeparateFilePath(wfilePath);
 }
 
 std::wstring TextureConverter::ConvertMultiByteStringToWideString(
@@ -50,4 +53,51 @@ std::wstring TextureConverter::ConvertMultiByteStringToWideString(
 
 void TextureConverter::SeparateFilePath(
 	const std::wstring& filePath) {
+	size_t pos1;
+	std::wstring exceptExt;
+
+	//区切り文字'.'が出てくる一番最後の部分を検索
+	pos1 = filePath.rfind('.');
+	//検索がヒットしたら
+	if (pos1 != std::wstring::npos) {
+		//区切り文字の後ろをファイル拡張子として保存
+		fileExt_ = filePath.substr(pos1 + 1, filePath.size() - pos1 - 1);
+
+		//区切り文字の前までを抜き出す
+		exceptExt = filePath.substr(0, pos1);
+	}
+	else {
+		fileExt_ = L"";
+		exceptExt = filePath;
+	}
+
+	//区切り文字'\\'が出てくる一番最後の部分を検索
+	pos1 = exceptExt.rfind('\\');
+	//検索がヒットしたら
+	if (pos1 != std::wstring::npos) {
+		//区切り文字の前をディレクトリパスとして保存
+		directoryPath_ = exceptExt.substr(0, pos1 + 1);
+
+		//区切り文字の後ろをファイル名として保存
+		fileName_ = exceptExt.substr(pos1 + 1, exceptExt.size() - pos1 - 1);
+
+		return;
+	}
+
+	//区切り文字'/'が出てくる一番最後の部分を検索
+	pos1 = exceptExt.rfind('/');
+	//検索がヒットしたら
+	if (pos1 != std::wstring::npos) {
+		//区切り文字の前をディレクトリパスとして保存
+		directoryPath_ = exceptExt.substr(0, pos1 + 1);
+
+		//区切り文字の後ろをファイル名として保存
+		fileName_ = exceptExt.substr(pos1 + 1, exceptExt.size() - pos1 - 1);
+
+		return;
+	}
+
+	//区切り文字がないので、ファイル名のみとして扱う
+	directoryPath_ = L"";
+	fileName_ = exceptExt;
 }
